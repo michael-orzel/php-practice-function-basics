fcd543ed3175c9f948cfcf72eb9fb78c328bda03
Michael Orzel <56739010+michael-orzel@users.noreply.github.com>
2020-10-23 12:43:35 -0400

    Create site.php

---
+++ site.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            function sayHello($name, $fg)
+            {
+                echo "Hello $name, you play $fg <br>";
+            }
+
+            sayHello("Daigo", "Street Fighter");
+            sayHello("G01", "Dragon Ball");
+            sayHello("Mang0", "Melee");
+        ?>
+    </body>
+</html>
